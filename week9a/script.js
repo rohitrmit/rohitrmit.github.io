eaca7e7fa832b7e19dfd3bd833b908fe002cc6dd
Rohit Ashok Khot <e06007@b00902020-26t.local>
2024-05-21 12:17:03 +1000

    added week9

---
+++ week9a/script.js
@@ -0,0 +1,19 @@
+const myButton = document.querySelector("#my-button");
+console.log(myButton);
+
+const sideDrawer = document.querySelector("#side-drawer");
+console.log(sideDrawer);
+
+let isOpened = false;
+
+myButton.addEventListener("click", toggleDrawer);
+
+function toggleDrawer(event) {
+  if (!isOpened) {
+    sideDrawer.style.translate = "200px";
+    isOpened = true;
+  } else {
+    sideDrawer.style.translate = "-200px";
+    isOpened = false;
+  }
+}
