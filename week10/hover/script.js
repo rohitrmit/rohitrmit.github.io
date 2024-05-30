6d13484ea561c60304ebc013d1502fa64292b8bc
rohitrmit <161539424+rohitrmit@users.noreply.github.com>
2024-05-30 16:21:39 +1000

    added week10

---
+++ week10/hover/script.js
@@ -0,0 +1,19 @@
+const catImg = document.querySelector("#cat-img");
+console.log(catImg);
+
+const moreInfo = document.querySelector("#more-info");
+console.log(moreInfo);
+
+catImg.addEventListener("mouseover", showInfo);
+catImg.addEventListener("mouseout", hideInfo);
+
+moreInfo.innerHTML = "<p>Add to cart </p>";
+
+function showInfo() {
+  moreInfo.classList.add("show");
+  //   moreInfo.style.backgroundColor = "coral";
+}
+
+function hideInfo() {
+  moreInfo.classList.remove("show");
+}
