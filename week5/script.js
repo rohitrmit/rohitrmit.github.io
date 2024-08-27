26d4bed877150959944413e975c54f72606ae874
rohitrmit <161539424+rohitrmit@users.noreply.github.com>
2024-08-27 16:25:53 +1000

    updated script

--- week5/script.js
+++ week5/script.js
@@ -138,10 +138,10 @@ for (let i = 0; i < shoppingCart.length; i++) {
 
 console.log("my shopping sum", sum);
 
-shoppingCart.forEach(calculateTotal);
-sum = 0;
-function calculateTotal(item) {
-  sum = sum + item.price;
-  console.log(item.price);
-}
-console.log("my shopping sum", sum);
+// shoppingCart.forEach(calculateTotal);
+// sum = 0;
+// function calculateTotal(item) {
+//   sum = sum + item.price;
+//   console.log(item.price);
+// }
+// console.log("my shopping sum", sum);
