566afd342612747b00cf82731b3d07da4f0160e0
rohitrmit <161539424+rohitrmit@users.noreply.github.com>
2025-04-10 09:57:27 +1000

    temp

---
+++ temp.js
@@ -0,0 +1,16 @@
+// variables  let and const
+// types of variables
+// numerical variable let a = 0 -  + - * /
+// string/ text variables let name = "Rohit"  'rohit'
+//  back ticks ` rohit
+//
+// `
+// boolean variables true false let isItRaining = false;
+//  objects
+//  let myRecord = { name: "rk", id: 1, isLocal: false};
+//  myRecord.name  = rk
+// arrays
+//  let numbers = [2,4,6,8,10];
+// arrays start at 0  numbers[2] = 6
+// if condition
+// for loops
