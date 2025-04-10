4c5a6a7474c191925f8c701b70d1ab9e2de23c19
rohitrmit <161539424+rohitrmit@users.noreply.github.com>
2025-04-10 14:02:03 +1000

    temp

---
+++ temp2.js
@@ -0,0 +1,15 @@
+// variables let const
+// types of variables
+// numerical variable let a = 10;  + - * /
+// string variables let name ="rohit" ' '
+//  `` multiline string or need to html and variables `
+// let myFavNumber = `my fav number
+//
+//
+// is ${a}` => my fav number is 10
+// boolean variables true or false isItRaining = false
+// objects
+// let myRecord = {name: "rk", id:1, isLocal:false};
+// myRecord.name = rk
+// arrays
+// let numbers = [2,4,6,8,10]
