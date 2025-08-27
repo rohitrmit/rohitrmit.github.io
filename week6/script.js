6f7ff9031f0b3d229db82660ccf73a4e8445f2c0
rohitrmit <161539424+rohitrmit@users.noreply.github.com>
2025-08-27 19:26:39 +1000

    week 6 mostly done!

--- week6/script.js
+++ week6/script.js
@@ -8,7 +8,14 @@ const firstPara = document.querySelector("p");
 console.log(firstPara);
 console.log(firstPara.textContent);
 firstPara.textContent = "This is my new para";
+firstPara.innerHTML += "<span> New element </span>";
 
+const mySection = document.querySelector("section");
+console.log(mySection);
+let mynewContent = `
+<h2>this is an image of cat </h2>
+<p> do you like it? </p>`;
+mySection.innerHTML += mynewContent;
 const h2Heading = document.querySelector("#second-heading");
 console.log(h2Heading);
 console.log(h2Heading.textContent);
@@ -38,4 +45,23 @@ function toggleMe() {
   const myImg = document.querySelector("img");
   console.log(myImg);
   myImg.classList.toggle("round");
+}
+
+const myImg = document.querySelector("img");
+console.log(myImg);
+myImg.addEventListener("mouseenter", addMe);
+myImg.addEventListener("mouseleave", removeMe);
+myImg.addEventListener("click", handleClick);
+
+function addMe() {
+  myImg.classList.add("round");
+}
+
+function removeMe() {
+  myImg.classList.remove("round");
+}
+
+function handleClick() {
+  console.log("did you just click me??");
+  topHeading.textContent = "My cat!";
 }
