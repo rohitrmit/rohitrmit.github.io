47fd85b40503515a601c1bb3684863bfa2f3c0c5
rohitrmit <161539424+rohitrmit@users.noreply.github.com>
2026-05-01 12:26:17 +1000

    week 7 done

--- week7/script.js
+++ week7/script.js
@@ -47,14 +47,35 @@ function makeItPop() {
   msg.textContent = "popping...";
 }
 
-const body = document.querySelector("body");
-body.addEventListener("keydown", playVideo);
+// const body = document.querySelector("body");
+// body.addEventListener("keydown", playVideo);
 
-function playVideo() {
-  if (event.code === "Space") {
-    // Prevent default behavior (like scrolling down the page)
-    event.preventDefault();
-    console.log("Spacebar was pressed!");
-    // myVideo.play();
+// function playVideo() {
+//   if (event.code === "Space") {
+//     // Prevent default behavior (like scrolling down the page)
+//     event.preventDefault();
+//     console.log("Spacebar was pressed!");
+//     // myVideo.play();
+//   }
+// }
+
+// access the play pause button
+const playPauseButton = document.querySelector("#play-pause-button");
+console.log(playPauseButton);
+
+//access the image
+const playPauseImg = document.querySelector("#play-pause-img");
+console.log(playPauseImg);
+
+// add a click event listener to the button
+playPauseButton.addEventListener("click", toggleVideo);
+
+function toggleVideo() {
+  if (myVideo.paused === true || myVideo.ended === true) {
+    myVideo.play();
+    playPauseImg.src = "https://img.icons8.com/ios-glyphs/30/pause--v2.png";
+  } else {
+    myVideo.pause();
+    playPauseImg.src = "https://img.icons8.com/ios-glyphs/30/play--v2.png";
   }
 }
